61393efacdd20f34ef7ea853336a52b914601d0e
David Byrns <david.byrns@crim.ca>
2017-06-22 21:04:00 -0400

    Add workflow schema validation

--- malleefowl/custom_workflow.py
+++ malleefowl/custom_workflow.py
@@ -69,6 +69,7 @@ Example:
 import argparse
 from multiprocessing import Manager
 
+import jsonschema
 from dispel4py.new import multi_process
 from dispel4py.workflow_graph import WorkflowGraph
 
@@ -80,6 +81,168 @@ from malleefowl.pe.generic_wps import GenericWPS, ParallelGenericWPS
 XML_DOC_READING_MAX_ATTEMPT = 5
 
 
+input_description_schema = {
+    "description": "Description of an input source",
+    "type": "object",
+    "required": ["task"],
+    "additionalProperties": False,
+    "properties": {
+        "task": {
+            "description": "Task name",
+            "type": "string"
+        },
+        "output": {
+            "description": "Task output name",
+            "type": "string"
+        },
+        "as_reference": {
+            "description": "Specify if the task output should be obtained as a reference or not",
+            "type": "boolean"
+        }
+    }
+}
+
+workflow_task_schema = {
+    "description": "Describe a WPS process task",
+    "type": "object",
+    "required": ["name", "url", "identifier"],
+    "additionalProperties": False,
+    "properties": {
+        "name": {
+            "description": "Unique name given to each workflow task",
+            "type": "string"
+        },
+        "url": {
+            "description": "Url of the WPS provider",
+            "type": "string"
+        },
+        "identifier": {
+            "description": "Identifier of a WPS process",
+            "type": "string"
+        },
+        "inputs": {
+            "description": "Dictionary of inputs that must be fed to the WPS process",
+            "type": "object",
+            "minItems": 1,
+            "patternProperties": {
+                ".*": {
+                    "oneOf": [
+                        {
+                            "description": "Data that must be fed to this input",
+                            "type": "string"
+                        },
+                        {
+                            "description": "Array of data that must be fed to this input",
+                            "type": "array",
+                            "minItems": 1,
+                            "items": {
+                                "type": "string"
+                            }
+                        }
+                    ]
+                }
+            }
+        },
+        "linked_inputs": {
+            "description": "Dictionary of dynamic inputs that must be fed to the WPS process and obtained by the output of other tasks",
+            "type": "object",
+            "minItems": 1,
+            "patternProperties": {
+                ".*": {
+                    "oneOf": [
+                        input_description_schema,
+                        {
+                            "description": "Array of input description that must be fed to this input",
+                            "type": "array",
+                            "minItems": 1,
+                            "items": input_description_schema
+                        }
+                    ]
+                }
+            }
+        },
+        "progress_range": {
+            "description": "Progress range to map the whole progress of this task",
+            "type": "array",
+            "minItems": 2,
+            "maxItems": 2,
+            "items": {
+                "type": "number",
+                "minimum": 0,
+                "maximum": 100
+            }
+        }
+    }
+}
+
+group_of_task_schema = {
+    "type": "object",
+    "description": "Describe a group of tasks to be run concurrently",
+    "required": ["name", "max_processes", "map", "reduce", "tasks"],
+    "additionalProperties": False,
+    "properties": {
+        "name": {
+            "description": "Group of task name",
+            "type": "string"
+        },
+        "max_processes": {
+            "description": "Number of processes to run concurrently to process the data",
+            "type": "number",
+            "minimum": 1
+        },
+        "map": {
+            "oneOf": [
+                input_description_schema,
+                {
+                    "description": "Array of data that has to be mapped directly",
+                    "type": "array",
+                    "minItems": 1,
+                    "items": {
+                        "type": "string"
+                    }
+                }
+            ]
+        },
+        "reduce": input_description_schema,
+        "tasks": {
+            "description": "Array of workflow task to run concurrently inside the group",
+            "type": "array",
+            "minItems": 1,
+            "items": workflow_task_schema
+        }
+    }
+}
+
+workflow_schema = {
+    "$schema": "http://json-schema.org/draft-04/schema#",
+    "title": "Workflow",
+    "description": "Advanced workflow schema",
+    "type": "object",
+    "required": ["name"],
+    "minProperties": 2,
+    "additionalProperties": False,
+    "properties": {
+        "name": {
+            "description": "Workflow name",
+            "type": "string"
+        },
+        "tasks": {
+            "description": "Array of workflow task",
+            "type": "array",
+            "minItems": 1,
+            "items": workflow_task_schema
+        },
+        "parallel_groups": {
+            "description": "Array of group of tasks being executed on multiple processes",
+            "type": "array",
+            "minItems": 1,
+            "items": group_of_task_schema
+        }
+    }
+}
+
+
+
 def run(workflow, monitor=None, headers=None):
     """
     Run the given workflow
@@ -88,6 +251,11 @@ def run(workflow, monitor=None, headers=None):
     :param headers: Headers to use when making a request to the WPS
     :return: A summary of the execution which is a list of all task's xml status
     """
+
+    try:
+        jsonschema.validate(workflow, workflow_schema)
+    except jsonschema.ValidationError as e:
+        raise Exception('The workflow is invalid : {0}'.format(str(e)))
 
     # Create WPS processes and append them in a task array
     tasks = []
