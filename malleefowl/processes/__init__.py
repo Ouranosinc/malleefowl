49f8d4670d726645780610c754eefc5ade648ad4
David Byrns <david.byrns@crim.ca>
2017-07-06 12:14:43 -0400

    Add a WPS process to get urls from thredds catalog

--- malleefowl/processes/__init__.py
+++ malleefowl/processes/__init__.py
@@ -1,7 +1,7 @@
 from .wps_esgflogon import MyProxyLogon
 from .wps_esgsearch import ESGSearchProcess
 from .wps_download import Download
-from .wps_thredds import ThreddsDownload
+from .wps_thredds import ThreddsDownload, ThreddsUrls
 from .wps_workflow import DispelWorkflow
 from .wps_custom_workflow import DispelCustomWorkflow
 
@@ -10,6 +10,7 @@ processes = [
     ESGSearchProcess(),
     Download(),
     ThreddsDownload(),
+    ThreddsUrls(),
     DispelWorkflow(),
     DispelCustomWorkflow(),
 ]
--- malleefowl/processes/wps_thredds.py
+++ malleefowl/processes/wps_thredds.py
@@ -54,3 +54,49 @@ class ThreddsDownload(Process):
             json.dump(obj=files, fp=fp, indent=4, sort_keys=True)
             response.outputs['output'].file = fp.name
         return response
+
+
+class ThreddsUrls(Process):
+    def __init__(self):
+        inputs = [
+            LiteralInput('url', 'URL',
+                         data_type='string',
+                         abstract="URL of the catalog.",
+                         min_occurs=1,
+                         max_occurs=1,
+                         ),
+        ]
+        outputs = [
+            ComplexOutput('output', 'Catalog file urls',
+                          abstract="JSON document with list of catalog file url.",
+                          as_reference=True,
+                          supported_formats=[Format('application/json')]),
+        ]
+
+        super(ThreddsUrls, self).__init__(
+            self._handler,
+            identifier="thredds_urls",
+            title="Get files url from Thredds Catalog",
+            version="0.5",
+            abstract="Get files url from Thredds Catalog and provides file list as JSON Document.",
+            metadata=[
+                Metadata('Birdhouse', 'http://bird-house.github.io/'),
+                Metadata('User Guide', 'http://malleefowl.readthedocs.io/en/latest/'),
+            ],
+            inputs=inputs,
+            outputs=outputs,
+            status_supported=True,
+            store_supported=True,
+        )
+
+    def _handler(self, request, response):
+        def monitor(message, progress):
+            response.update_status(message, progress)
+
+        import threddsclient
+        urls = threddsclient.download_urls(request.inputs['url'][0].data)
+
+        with open('out.json', 'w') as fp:
+            json.dump(obj=urls, fp=fp, indent=4, sort_keys=True)
+            response.outputs['output'].file = fp.name
+        return response
